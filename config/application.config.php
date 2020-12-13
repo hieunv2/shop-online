31dec9409ebaac70ab6079fcc7c00453abf75c60
Nguyễn Hiếu <vanhieu13546@gmail.com>
2020-12-13 08:54:40 +0700

    init

---
+++ config/application.config.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    // to start you need manually create the user in your MySQL:
+    // CREATE USER 'blog'@'localhost' IDENTIFIED BY 'testpasBlogsword';
+
+    // database connection parameters
+    'connection' => [
+        'database_type' => 'mysqli',
+        'params' => [
+            'host' => 'localhost',
+            'port' => '3306',
+            'user' => 'root',
+            'password' => '123456',
+            'dbname' => 'dan',
+        ]
+    ]
+];
